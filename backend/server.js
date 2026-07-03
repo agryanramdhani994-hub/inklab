c3458ff534d8e42c6a1572e2c754007a58f5847e
agryanramdhani994-hub <agryanramdhani994@gmail.com>
2026-07-03 08:07:45 +0700

    Fix frontend path

--- backend/server.js
+++ backend/server.js
@@ -30,22 +30,22 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/orders", orderRoutes);
 
 // Frontend
-app.use(express.static(path.join(__dirname, "../frontend")));
+app.use(express.static(path.join(__dirname, "frontend")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
 
 console.log("__dirname =", __dirname);
-console.log("frontend =", path.join(__dirname, "../frontend"));
-console.log("index =", path.join(__dirname, "../frontend/index.html"));
+console.log("frontend =", path.join(__dirname, "frontend"));
+console.log("index =", path.join(__dirname, "frontend", "index.html"));
 
 const fs = require("fs");
 
 console.log(
-    fs.existsSync(path.join(__dirname, "../frontend/profile.html"))
+    fs.existsSync(path.join(__dirname, "frontend", "profile.html"))
 );
 
 app.listen(PORT, () => {
